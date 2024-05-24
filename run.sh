40a4b65354d363fdd7636e1bc195a9984ca895b6
Andrei Timotin <timotin.andrei@justconsult.md>
2024-05-24 14:46:24 +0300

    feat: Updated script to check if PM2 service is online and launch it if not

--- run.sh
+++ run.sh
@@ -15,6 +15,9 @@ else
         echo "Folderul $PM2_FOLDER este gol. Îl ștergem acum..."
         rmdir "$PM2_FOLDER"
         echo "Folderul a fost șters."
+        mkdir -p "$PM2_FOLDER"
+        cd "$PM2_FOLDER" || exit
+        npm i pm2
     else
         echo "Folderul $PM2_FOLDER nu este gol."
     fi
@@ -23,6 +26,14 @@ fi
 if [ -d "$APP_FOLDER" ]; then
     cd "$APP_FOLDER" || exit
     git pull
+    if pm2 status | grep -q "online"; then
+        echo "PM2 rulează și toate procesele sunt online."
+    else
+        echo "PM2 nu rulează. Pornim PM2..."
+        pm2 start index.js --instances 1
+        pm2 save
+        echo "Starea a fost salvată cu succes."
+    fi
 else
     cd /c || exit
     git clone https://github.com/sibeor/recorder-audio.git
@@ -33,7 +44,7 @@ else
     pm2 start index.js --instances 1
     pm2 save
 fi
-
+sleep 3
 pm2 resurrect
 
 echo "Operațiunea a fost finalizată."
